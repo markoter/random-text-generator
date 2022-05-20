7925c77149d7c166dcf6907b5334f8506f06d867
markoter <61563247+markoter@users.noreply.github.com>
2022-05-20 22:20:21 +0200

    get words from txt file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var path = @"C:\Users\naard\Dev\Projects\random-text-generator\cicero.txt";
+
+string latinText = File.ReadAllText(path);
+string[] words = latinText.Split(' ');
+
+System.Console.WriteLine(words[1]);
+
