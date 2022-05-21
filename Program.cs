ad33f82114cecc0daddaf9caee135ccd661c6563
markoter <61563247+markoter@users.noreply.github.com>
2022-05-21 12:01:52 +0200

    generate sentence from random list of words

--- Program.cs
+++ Program.cs
@@ -6,10 +6,24 @@ var path = @"C:\Users\naard\Dev\Projects\random-text-generator\cicero.txt";
 string latinText = File.ReadAllText(path);
 string[] words = latinText.Split(' ');
 
-System.Console.WriteLine(words[1]);
+System.Console.WriteLine($"len of words is {words.Count()}");
 
-RandomSentence(words);
+var sentence = RandomSentence(words);
 
-static void RandomSentence(string[] words) {
-    
+System.Console.WriteLine(sentence);
+
+string RandomSentence(string[] words)
+{
+    var rnd = new Random();
+    var sentenceLength = rnd.Next(3, 10);
+
+    var wordsOfSentence = new string[sentenceLength];
+
+    for (int i= 0; i < sentenceLength; i++) {
+        wordsOfSentence[i] = words[rnd.Next(words.Count())];
+    }
+    var sentence = string.Join(" ", wordsOfSentence);
+
+
+    return sentence;
 }
